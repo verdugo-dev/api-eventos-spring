a31efe894e0c1bcec741d77a7fede41b12b4cb29
Carlos Verdugo <carlos.versa99@gmail.com>
2026-08-18 16:58:34 -0700

    Adding class entity event

---
+++ src/main/java/com/gestion/eventos/api/domain/Event.java
@@ -0,0 +1,21 @@
+package com.gestion.eventos.api.domain;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "events")
+public class Event {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDate date;
+    private String location;
+}
