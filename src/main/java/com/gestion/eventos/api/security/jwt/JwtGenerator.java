2670ee79aa2706ec093e6657d0932c1e14d983bf
Carlos Verdugo <carlos.versa99@gmail.com>
2026-08-21 16:57:04 -0700

    Adding key for jwt generator

--- src/main/java/com/gestion/eventos/api/security/jwt/JwtGenerator.java
+++ src/main/java/com/gestion/eventos/api/security/jwt/JwtGenerator.java
@@ -2,10 +2,15 @@ package com.gestion.eventos.api.security.jwt;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
 @Component
@@ -15,10 +20,24 @@ public class JwtGenerator {
     private String jwtSecret;
 
     @Value("${jwt.expiration}")
-    private long jwgExpiration;
+    private long jwtExpiration;
 
-    private Key getSigningKey() {
+    private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
     }
 
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpiration);
+
+        String token = Jwts.builder()
+            .subject(username)
+            .issuedAt(new Date())
+            .expiration(expireDate)
+            .signWith(getSigningKey(), Jwts.SIG.HS512)
+            .compact();
+
+        return token;
+    }
 }
