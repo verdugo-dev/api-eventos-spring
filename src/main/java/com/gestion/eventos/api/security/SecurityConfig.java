b4455453db869bec1d96ae625a4cf0830c59c7bd
Carlos Verdugo <carlos.versa99@gmail.com>
2026-08-24 10:58:38 -0700

    Change filterChain in security config

--- src/main/java/com/gestion/eventos/api/security/SecurityConfig.java
+++ src/main/java/com/gestion/eventos/api/security/SecurityConfig.java
@@ -2,15 +2,20 @@ package com.gestion.eventos.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.gestion.eventos.api.security.jwt.JwtAuthEntryPoint;
+import com.gestion.eventos.api.security.jwt.JwtAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,19 +24,28 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         http
             .csrf(AbstractHttpConfigurer::disable)
+            .exceptionHandling(exception -> 
+                exception.authenticationEntryPoint(jwtAuthEntryPoint)
+            )
+            .sessionManagement(session -> 
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
             .authorizeHttpRequests(auth -> 
-                auth.requestMatchers("/api/v1/auth/**")
-                    .permitAll()
+                auth.requestMatchers("/api/v1/auth/**").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/v1/events/**").permitAll()
                     .anyRequest()
                     .authenticated()
-            )
-            .httpBasic(Customizer.withDefaults());
+            );
+            // .httpBasic(Customizer.withDefaults());
 
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
