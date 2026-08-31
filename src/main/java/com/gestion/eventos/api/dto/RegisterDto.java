d1c42aebc91ef0a5423a1c8cf8b326ca02e49285
Carlos Verdugo <carlos.versa99@gmail.com>
2026-08-31 12:54:04 -0700

    Adding validation in registerDto and map roles in User Mapper

--- src/main/java/com/gestion/eventos/api/dto/RegisterDto.java
+++ src/main/java/com/gestion/eventos/api/dto/RegisterDto.java
@@ -1,5 +1,12 @@
 package com.gestion.eventos.api.dto;
 
+import java.util.Set;
+
+import com.gestion.eventos.api.domain.Role;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +15,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterDto {
+    @NotBlank(message = "El nombre de usuario no puede estar vacio.")
+    @Size(min = 4, max = 20, message = "El nombre de usuario debe tener entre 4 y 20 caracteres.")
     private String username;
+
+    @NotBlank(message = "La contraseña no puede estar vacia.")
+    @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
     private String password;
+
+    @NotBlank(message = "El email no puede estar vacio.")
+    @Email(message = "Debe ser una direccion de correo electronico valida")
     private String email;
+
+    @NotBlank(message =  "El nombre no puede estar vacio")
     private String name;
+
+    private Set<Role> roles;
 }
--- src/main/java/com/gestion/eventos/api/mapper/UserMapper.java
+++ src/main/java/com/gestion/eventos/api/mapper/UserMapper.java
@@ -1,16 +1,50 @@
 package com.gestion.eventos.api.mapper;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import com.gestion.eventos.api.domain.Role;
 import com.gestion.eventos.api.domain.User;
 import com.gestion.eventos.api.dto.RegisterDto;
+import com.gestion.eventos.api.repository.RoleRepository;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper {
+public abstract class UserMapper {
+
+    protected final RoleRepository roleRepository;
+
+    UserMapper(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
 
     @Mapping(target = "password", ignore = true)
     @Mapping(target = "id", ignore = true)
-    User registerDtoToUser(RegisterDto registerDto);
+    public abstract User registerDtoToUser(RegisterDto registerDto);
+
+    public Set<Role> mapRoleStringsToRoles(Set<String> roleNames) {
+        if (roleNames == null || roleNames.isEmpty()) {
+            return roleRepository.findByName("ROLE_USER")
+                .map(Collections::singleton)
+                .orElseThrow(
+                    () -> new RuntimeException("Error: Rol 'ROLE_USER' no encontrado en la base de datos. " +
+                        "Asegurate de que el rol ROLE_USER exista al iniciar la aplicacion"
+                    )
+                );
+        }
+
+        return roleNames.stream()
+            .map(
+                roleName -> roleRepository.findByName(roleName)
+                    .orElseThrow(
+                        () -> new RuntimeException("Error: Rol no encontrado: " + roleName)
+                    )
+            )
+            .collect(Collectors.toSet());
+
+    }
 
 }
